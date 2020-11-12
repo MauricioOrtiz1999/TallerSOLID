42d249d1ad8bb4af9a1ee03581978888472f40b4
abbarco <68168827+abbarco@users.noreply.github.com>
2020-11-11 20:50:49 -0500

    Modificacion clase Aderezo

---
+++ src/Adicionales/Frutilla.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Adicionales;
+
+/**
+ *
+ * @author anthony
+ */
+public class Frutilla extends Aderezo{
+
+    public Frutilla(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        super.nombre=nombre;
+    }
+    
+    
+}
---
+++ src/Adicionales/Malvavisco.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Adicionales;
+
+/**
+ *
+ * @author anthony
+ */
+public class Malvavisco extends Aderezo{
+
+    public Malvavisco(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        super.nombre=nombre;
+    }
+    
+}
